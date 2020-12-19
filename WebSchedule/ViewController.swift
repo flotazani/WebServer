4d2c1a699786714ce11d46f6d042ead446d07467
Andrei Konovalov <flotazani@gmail.com>
2020-12-19 20:42:42 +0300

    Initial Commit

---
+++ WebSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebSchedule
+//
+//  Created by Andrei Konovalov on 19.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
