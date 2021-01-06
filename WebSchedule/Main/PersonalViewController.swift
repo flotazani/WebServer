42243dc872fc6c774c8a2ccc7e897c4bdeec0f15
Andrei Konovalov <flotazani@gmail.com>
2021-01-06 22:30:09 +0300

    added main app ViewControllers

---
+++ WebSchedule/Main/PersonalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalViewController.swift
+//  WebSchedule
+//
+//  Created by Andrei Konovalov on 06.01.2021.
+//
+
+import UIKit
+
+class PersonalViewController: BasicViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
