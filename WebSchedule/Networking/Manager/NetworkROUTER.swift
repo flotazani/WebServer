e14335654dd397a1f84704822dfd3a85f61ae6c1
Andrei Konovalov <flotazani@gmail.com>
2021-02-27 20:14:19 +0300

    weird mistake

---
+++ WebSchedule/Networking/Manager/NetworkROUTER.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkService.swift
+//  NetworkLayer
+//
+//  Created by Malcolm Kumwenda on 2018/03/07.
+//  Copyright © 2018 Malcolm Kumwenda. All rights reserved.
+//
+
+import Foundation
+
+public typealias NetworkRouterCompletion = (_ data: Data?,_ response: URLResponse?,_ error: Error?)->()
+
+protocol NetworkRouter: class {
+    associatedtype EndPoint: EndPointType
+    func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion)
+    func cancel()
+}
